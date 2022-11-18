f8665dba5ec8e523e6ab0220055ea63d4137997b
Dheeraj V <dvijayen@contractor.jcp.com>
2022-11-17 17:49:32 -0800

    filterby changes

--- src/app/components/discover-movie/discover-movie.component.ts
+++ src/app/components/discover-movie/discover-movie.component.ts
@@ -10,27 +10,53 @@ import { DiscoverMovieService } from 'src/app/service/discover-movie.service';
 export class DiscoverMovieComponent implements OnInit {
  
   movieList:Movies = {} as Movies;
+  filteredMovieList:Movies = {} as Movies;
 
   constructor(private discoverMovieService:DiscoverMovieService) { }
 
   ngOnInit(): void {
     this.discoverMovieService.getMovieList().subscribe((response:Movies)=>{
       console.log(response.results);
-      this.movieList = response;
+      this.movieList = JSON.parse(JSON.stringify(response));
+      this.filteredMovieList = JSON.parse(JSON.stringify(response));
     })
    
   }
 
    movieByReview():any{
-    const res = this.movieList.results.sort((a, b) => a.vote_average > b.vote_average? -1 : 1);
-     this.movieList.results = res;
+    const res = this.filteredMovieList.results.sort((a, b) => a.vote_average > b.vote_average? -1 : 1);
+     this.filteredMovieList.results = res;
      
   }
+
+  movieByPopularity():any{
+    const res = this.filteredMovieList.results.sort((a, b) => a.popularity > b.popularity? -1 : 1);
+     this.filteredMovieList.results = res;
+     
+  }
+  movieByReleaseDate():any{
+    const res = this.filteredMovieList.results.sort((a, b) => a.release_date > b.release_date? -1 : 1);
+     this.filteredMovieList.results = res;
+     
+  }
+
   onChange(e:any):void {
     console.log(e.target.value);
-    this.movieByReview();
-    
+    const selectedFilter = e.target.value;
 
+    if(selectedFilter === 'review'){
+      this.movieByReview();
+    }
+    else if(selectedFilter === 'popularity'){
+      this.movieByPopularity()
+    }
+    else if(selectedFilter === 'releasedate'){
+      this.movieByReleaseDate()
+    }
+    else if(selectedFilter==='default'){
+      console.log(this.movieList.results)
+      this.filteredMovieList = this.movieList;
+    }
   }
 
 }
